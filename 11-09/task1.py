5e524240612bad4fdb408e389ef3333543850891
Naveenkumar579 <144647644+Naveenkumar579@users.noreply.github.com>
2023-09-11 12:25:02 +0530

    Add files via upload

---
+++ 11-09/task1.py
@@ -0,0 +1,12 @@
+Name=("Naveenkumar Vijayakumar")
+Address=("Poosarikadu  vembadithalam salem-637504")
+Class=("Msc computer science")
+T=80
+E=80
+M=90
+Phy=90
+Che=80
+Bio=95
+Total=T+E+M+Phy+Che+Bio
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Bio,"My total mark",Total,"Avg",Avg)
